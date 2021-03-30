9b345cef0161bebb3c2ba7962e6550af817feff9
Drew Parsons <drewmparsons@gmail.com>
2021-03-30 14:09:49 -0400

    first  commit

---
+++ src/myFirstPackage/MyFirstClass.java
@@ -0,0 +1,22 @@
+package myFirstPackage;
+
+public class MyFirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// main method inherits from Thread. 
+		//entry point  for  the  project
+		String message = "hello";
+		System.out.print(message);
+	
+
+		int year = 2024;
+		boolean isLeapYear = (year%4 ==0) && (year%100 !=0);
+		System.out.println("Year" + isLeapYear);
+		
+		
+
+	}
+
+}
